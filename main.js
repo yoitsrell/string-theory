fad89c87c96bd5ecd0b7ae27a43f26dfbf0243d7
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-19 12:02:09 -0500

    80% done

--- main.js
+++ main.js
@@ -1,22 +1,99 @@
-function crazyCase() {
+//function cap2ndletter(str){
+  // let answer = " "
+  // for (let i = 0; i < str.length; i++){
+  //  if (i==1){
+
+    //}
+    // answer = answer + str[i]
+  // }
+  // return answer
+
+
+function crazyCase(str) {
+  let answer = '';
+  for (let i = 0; i < str.length; i++){
+      if (i % 2 === 0) {
+      answer = answer + str[i].toLowerCase();
+    }   else if (i % 2 === 1) {
+        answer = answer + str[i].toUpperCase();
+    }
+  }
+  return answer;
 }
 
-function ciEmailify() {
+function ciEmailify(str) {
+  let answer = '';
+  for (let i = 0 ; i < str.length; i++){
+    if (str[i] === ' ') { 
+      answer = answer + '.';
+    } else {
+      answer = answer + str[i]; 
+    }
+  }
+return answer.toLowerCase() + '@codeimmersives.com';
 }
 
-function exclaim() {
+
+function exclaim(str) {  
+  let answer = '';
+  for (let i = 0 ; i < str.length; i++){
+    if (str[i] === '?' || str[i] === '.') { 
+      answer = answer + '!';
+    } else {
+      answer = answer + str[i]; 
+    }
+  }
+return answer;
 }
 
-function reverse() {
+function reverse(str) {  
+  let reversed = "";    
+for (var i = str.length - 1; i >= 0; i--)
+{        
+  reversed += str[i];
+}    
+return reversed;
 }
 
-function crazyCase2ReturnOfCrazyCase() {
+
+
+function crazyCase2ReturnOfCrazyCase(str) {
+  let answer = '';
+  
+  
+  for (let i = 0; i < str.length; i++){
+      if (i % 2 === 0) {
+      answer = answer + str[i].toLowerCase();
+    }   else if (i % 2 === 1) {
+        answer = answer + str[i].toUpperCase();
+    }
+  }
+  return answer;
 }
 
-function titleCase() {
-}
 
-function onlyVowels() {
+function titleCase(str) {
+//   let output = ""
+
+//   for (let i=0; i<str.length; i++){
+//     let ch = str[i]
+//     if(ch === ch.toUpperCase()){
+//       output += ch.toLowerCase()
+//     }else{
+//       output += ch.toUpperCase()
+//     }
+//   }
+//   return output
+// }
+
+function onlyVowels(str) {  
+let answer = ''
+for (i = 0; i < str.length; i++) {
+  if (('aeiouAEIOU'.includes(str.charAt(i))) === true) {
+    answer = answer + str[i];
+  }
+} 
+return answer;
 }
 
 function crazyCase3SonOfCrazyCase() {
@@ -32,4 +109,4 @@ module.exports = {
   titleCase,
   onlyVowels,
   crazyCase3SonOfCrazyCase
-}
+}}
